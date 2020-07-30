ffdd1fc7774a79357f17c35b0c90858dccb8e949
anthonybryanm.pagarigan <anthonypagarigan1105@gmail.com>
2020-07-30 15:35:30 +0800

    update

--- lib/components/rounded_button.dart
+++ lib/components/rounded_button.dart
@@ -1,33 +1,32 @@
 import 'package:flutter/material.dart';
 
-class RoundButton extends StatelessWidget {
-  RoundButton(
-      {@required this.onPressed, @required this.text, @required this.color});
+class RoundedButton extends StatelessWidget {
+  RoundedButton(
+      {@required this.title, @required this.colour, @required this.onPressed});
 
   final Function onPressed;
-  final Text text;
-  final Color color;
+  final String title;
+  final Color colour;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: 16.0),
-            child: Material(
-              elevation: 5.0,
-              color: color,
-              borderRadius: BorderRadius.circular(30.0),
-              child: MaterialButton(
-                onPressed: onPressed,
-                minWidth: 200.0,
-                height: 42.0,
-                child: text,
-              ),
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: Material(
+        elevation: 5.0,
+        color: colour,
+        borderRadius: BorderRadius.circular(30.0),
+        child: MaterialButton(
+          onPressed: onPressed,
+          minWidth: 200.0,
+          height: 42.0,
+          child: Text(
+            title,
+            style: TextStyle(
+              color: Colors.white,
             ),
           ),
-        ],
+        ),
       ),
     );
   }
